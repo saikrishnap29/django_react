4a73a389fc4ade17eb5f0796d71cc04022c8a3b4
saikrishnap29 <saikrishnap29@gmail.com>
2020-05-16 22:26:03 +0530

    urls added

--- djrec/urls.py
+++ djrec/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from.import views
+from rest_framework import routers
+from.api import LeadViewset
+router = routers.DefaultRouter()
+router.register('api/leads',LeadViewset,'leads')
+urlpatterns = router.urls
