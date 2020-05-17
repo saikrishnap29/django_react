33f974a9e8415a1337f3ad4aa952dc1924f4c240
saikrishnap29 <saikrishnap29@gmail.com>
2020-05-17 09:24:34 +0530

    display app and static

--- django_react/urls.py
+++ django_react/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("djrec.urls"))
+    path('',include("djrec.urls")),
+    path('', include('display.urls')),
    ]
