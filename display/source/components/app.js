7d95ea3d3db57701911b4910b1d983a360abd508
saikrishnap29 <saikrishnap29@gmail.com>
2020-05-17 12:14:19 +0530

    component leads added

--- display/source/components/app.js
+++ display/source/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-
+import Header from './layout/Header';
 class App extends Component {
   constructor(props) {
     super(props);
